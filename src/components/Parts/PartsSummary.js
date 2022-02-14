988dd3ebc4fc69d77c23101e57ff0fdaeba5f7e5
Brando Lugo <brando.lugo@gmail.com>
2022-02-14 16:15:30 -0600

    latest

---
+++ src/components/Parts/PartsSummary.js
@@ -0,0 +1,11 @@
+import classes from './PartsSummary.module.css';
+
+export default function PartsSummary() {
+  return (
+    <section className={}>
+      <h2>Great parts sent to you.</h2>
+      <p></p>
+      <p></p>
+    </section>
+  );
+}
